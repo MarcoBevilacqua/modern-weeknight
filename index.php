cb715f9a713aa213df1d6a7b2181c92d67b11b18
Marco Francesco Bevilacqua <marco.bevilacqua@finconsgroup.local>
2020-03-25 18:54:21 +0100

    bit of Index style

--- index.php
+++ index.php
@@ -1,45 +1,35 @@
 <html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <title>Modern Weeknight</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <link rel="stylesheet" href="./style.css">
-  </head>
-  <body>
-    <div class="container">
-      <div class="row">
-        <div class="col text-center">
-          <h2 class="title form-title">Ugly lines beautifier</h2>
-        </div>        
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <title>Modern Weeknight</title>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+  <link rel="stylesheet" href="./style.css">
+</head>
+
+<body>
+  <div class="container">
+    <div class="row">
+      <div class="col text-center">
+        <h2 class="title form-title">Ugly lines beautifier</h2>
       </div>
     </div>
-  </body>
-</html>
-
-<form id="form_102509" class="appnitro" enctype="multipart/form-data" method="post" action="./submit.php">
-      <div class="form_description">
-  <h2>Untitled Form</h2>
-  <p>This is your form description. Click here to edit.</p>
-</div>
-  <ul >
-    <div>
-      <input id="te_1" name="te_1" class="element text" type="text"/>
+    <div class="row">      
+      <div class="col">
+        <form id="mw_form" class="mw_form form" enctype="multipart/form-data" method="post" action="./src/submit.php">
+          <div class="form-group">
+            <label for="mw_file">Upload a File</label>
+            <input id="mw_file" name="mw_file" class="form-control" type="file" />
+          </div>
+          <div class="col text-center">
+            <input id="saveForm" class="btn btn-primary" type="submit" name="submit" value="Parse" />
+          </div>        
+        </form>        
+      </div>
     </div>
-
-      <li id="li_1" >
-<label class="description" for="file_1">Upload a File </label>
-<div>
-  <input id="file_1" name="file_1" class="element file" type="file"/>
-</div>
-</li>
-
-      <li class="buttons">
-      <input type="hidden" name="form_id" value="102509" />
-
-    <input id="saveForm" class="button_text" type="submit" name="submit" value="Submit" />
-</li>
-  </ul>
-</form>
+  </div>
+</body>
+</html>
